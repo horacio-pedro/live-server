f581bae9cb0376d6e65af1130da9972d4a4ced6b
Horácio Pedro <46960527+horacio-pedro@users.noreply.github.com>
2019-09-19 12:08:37 +0100

    commit

--- start/routes.js
+++ start/routes.js
@@ -22,3 +22,6 @@ Route.get('/', () => {
 
 Route.post('/users', 'UserController.create')
 Route.post('/sessions', 'SessionController.create')
+Route.resource('properties', 'PropertyController')
+  .apiOnly()
+  .middleware('auth')
